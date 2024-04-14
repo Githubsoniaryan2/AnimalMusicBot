bf1d90738cf2c65177f95932e620fafb85c65239
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2024-04-14 11:43:54 +0530

    Update play.py

--- BRANDEDKING/utils/inline/play.py
+++ BRANDEDKING/utils/inline/play.py
@@ -68,7 +68,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="🌹Eʀʀᴏʀ🌹", url="https://t.me/systum30",
+                text="🌹Eʀʀᴏʀ🌹", url="https://t.me/ll_KANNU_ll",
             ),
             InlineKeyboardButton(
                 text="❤️‍🔥Aɴɪᴍᴀʟ❤️‍🔥", url="https://t.me/AnimalJanwarrrr",
@@ -98,7 +98,7 @@ def stream_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🌹Eʀʀᴏʀ🌹", url="https://t.me/systum30",
+                text="🌹Eʀʀᴏʀ🌹", url="https://t.me/ll_KANNU_ll",
             ),
             InlineKeyboardButton(
                 text="❤️‍🔥Aɴɪᴍᴀʟ❤️‍🔥", url="https://t.me/AnimalJanwarrrr",
