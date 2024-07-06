cf3f425e5d68aeecfde706c0ec5392d005e5c397
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2024-07-06 17:03:17 +0530

    Update play.py

--- BRANDEDKING/utils/inline/play.py
+++ BRANDEDKING/utils/inline/play.py
@@ -79,7 +79,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                  text="💌Sᴜᴘᴘᴏʀᴛ💌", url="https://t.me/+vUKdfE7LVQc0NjY9",
             ),
             InlineKeyboardButton(
-                text="🦋Cʜᴀɴɴᴇʟ🦋", url="https://t.me/itsRareBeautySelenaGomez",
+                text="🦋Cʜᴀɴɴᴇʟ🦋", url="https://t.me/SHE_FEELZ",
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -109,7 +109,7 @@ def stream_markup(_, chat_id):
                 text="💌Sᴜᴘᴘᴏʀᴛ💌", url="https://t.me/+vUKdfE7LVQc0NjY9",
             ),
             InlineKeyboardButton(
-                text="🦋Cʜᴀɴɴᴇʟ🦋", url="https://t.me/itsRareBeautySelenaGomez",
+                text="🦋Cʜᴀɴɴᴇʟ🦋", url="https://t.me/SHE_FEELZ",
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
